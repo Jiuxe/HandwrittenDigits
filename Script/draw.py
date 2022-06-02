02561d148feb6d2eca866f23ddbb93cf98bdeedd
J. Jesús González Mairena <jiuxej@gmail.com>
2022-06-02 19:13:11 +0200

    Ultimos retoques para GUI y añadido label de prediccion para ver el resultado.

--- Script/draw.py
+++ Script/draw.py
@@ -1,7 +1,6 @@
-from tkinter import Canvas, Tk, Button, messagebox, filedialog, Scale, HORIZONTAL, ALL, DISABLED, NORMAL
+from tkinter import Canvas, Tk, Button, ALL, DISABLED, NORMAL, Label
 import PIL.ImageGrab as ImageGrab
 import numpy as np
-import matplotlib.pyplot as plt
 import main
 
 line_x = 0
@@ -21,10 +20,6 @@ def line(event):
 
 def clear():
     canvas.delete(ALL)
-
-def quit():
-    window.destroy()
-    window.quit()
 
 def getDraw():
     x = window.winfo_rootx() + canvas.winfo_x()
@@ -47,28 +42,31 @@ def trainingModel():
 
 def drawAnalysis():
     image = getDraw()
-    print(main.evaluateOneElement(image))
+    labelResult.config(text="Pred: " + str(main.evaluateOneElement(image)))
 
 window = Tk("HandWrittenDigits")
 window.config(height=500,width=500,bg="black")
 
 window.rowconfigure(1)
-window.columnconfigure(2)
+window.columnconfigure(3)
 
 canvas = Canvas(window, bg="black")
-canvas.grid(row=0,columnspan=3)
+canvas.grid(row=0,columnspan=4)
 
 canvas.bind('<Button-1>',line_xy)
 canvas.bind('<B1-Motion>',line)
 
-clearButton = Button(window, command=clear, text="Limpiar", bg='red')
+clearButton = Button(window, command=clear, text="Limpiar", bg='red4', font=('', 10, 'bold'))
 clearButton.grid(row=1, column=0, sticky='ew')
 
-analysisButton = Button(window, command=drawAnalysis, text="Analizar dibujo", bg='green')
+analysisButton = Button(window, command=drawAnalysis, text="Analizar dibujo", bg='OliveDrab1', font=('', 10, 'bold'))
 analysisButton.grid(row=1, column=1, sticky='ew')
 analysisButton.config(state=DISABLED)
 
-trainingButton = Button(window, command=trainingModel, text="Entrenar Modelo", bg='blue')
+trainingButton = Button(window, command=trainingModel, text="Entrenar Modelo", bg='RoyalBlue3', font=('', 10, 'bold'))
 trainingButton.grid(row=1, column=2, sticky='ew')
+
+labelResult = Label(window, text="Pred: x", bg="steelblue", font=('', 10, 'bold'))
+labelResult.grid(row=1, column=3, sticky='ew')
 
 window.mainloop()
